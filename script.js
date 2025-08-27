d21ab709c72b3b3c73b8bf3f94b955cdc9be5425
Paulo Henrique Warmling <37279@UNIDAVI.LOCAL>
2025-08-26 21:27:33 -0300

    melhorando logica teclado - wip

--- script.js
+++ script.js
@@ -3,22 +3,31 @@ document.addEventListener("DOMContentLoaded", () => {
     const tela = document.querySelector('.tela')
     botoes.forEach((btn) => {
         btn.addEventListener('click', ()=>{
-            let operacao = []
+            let operacao = [7]
             let numeroAtual = ""
             if(isNaN(btn.textContent)) {
-                operacao = btn.textContent
+                
             } else {
                 numeroAtual += btn.textContent
                 console.log(numeroAtual)
             }
             
 
-            if(tela.textContent === '0') {
+            if(tela.textContent === '0' && btn.textContent !== "c" && btn.textContent !== ".") {
                 tela.textContent = btn.textContent
             } else if(btn.textContent === 'c') {
                 tela.textContent = '0'
-            } else {
+            } else if(isNaN(btn.textContent) && btn.textContent !== "."){
+                tela.textContent = btn.textContent
+            } else if(isNaN(tela.textContent)) {
+                tela.textContent = btn.textContent
+            } else if(btn.textContent === ".") {
                 tela.textContent += btn.textContent
+            }
+            
+            else {
+                tela.textContent += btn.textContent
+               
             }
             
         })
