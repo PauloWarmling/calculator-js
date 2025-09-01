2c534cfe99bfa7753d9a19eec16ef453ff5c9cee
PAULO HENRIQUE WARMLING <paulo.warmling@edu.sc.senai.br>
2025-09-01 11:03:25 -0300

    Lógica para cálculo

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
+let a = null
+let b = null
+let operacao = null
+
 document.addEventListener("DOMContentLoaded", () => {
     const botoes = document.querySelectorAll('.buttons button')
     const tela = document.querySelector('.tela')
     botoes.forEach((btn) => {
         btn.addEventListener('click', ()=>{
-            let operacao = [7]
-            let numeroAtual = ""
-            if(isNaN(btn.textContent)) {
-                
+            if(!isNaN(btn.textContent)) {
+                handleNumber(btn.textContent)  
             } else {
-                numeroAtual += btn.textContent
-                console.log(numeroAtual)
+                handleSymbol(btn.textContent)
             }
-            
-
-            if(tela.textContent === '0' && btn.textContent !== "c" && btn.textContent !== ".") {
-                tela.textContent = btn.textContent
-            } else if(btn.textContent === 'c') {
-                tela.textContent = '0'
-            } else if(isNaN(btn.textContent) && btn.textContent !== "."){
-                tela.textContent = btn.textContent
-            } else if(isNaN(tela.textContent)) {
-                tela.textContent = btn.textContent
-            } else if(btn.textContent === ".") {
-                tela.textContent += btn.textContent
-            }
-            
-            else {
-                tela.textContent += btn.textContent
-               
-            }
-            
         })
     });
     
+    function handleNumber(number){
+        if(tela.textContent === "0" && number !== "."){
+            tela.textContent = number
+        } else if(isNaN(tela.textContent)) {
+            tela.textContent = number
+        } else if(!isNaN(tela.textContent) || number === "." || !isNaN(number)) {
+            tela.textContent += number
+        } 
+    }
+
+    function handleSymbol(symbol) {
+        if(symbol === "c"){
+            tela.textContent = "0"
+            a = null
+            b = null
+            operacao = null
+        } else if(symbol === "=") {
+            debugger
+            if(a !== null && operacao !== null) {
+                b = Number(tela.textContent) // pega o segundo número
+                tela.textContent = handleMath(a, b, operacao)
+                a = Number(tela.textContent) // guarda resultado para continuar
+                b = null
+                operacao = null
+            }
+            handleMath(a, b, operacao)
+        } else if(symbol === "."){
+            handleNumber(symbol)
+        } else {
+            a = Number(tela.textContent) // guarda primeiro número
+            operacao = symbol            // guarda operador
+            tela.textContent = symbol    // mostra operador na tela
+        }
+    }
+
+    function handleMath(a, b, operacao) {
+        switch (operacao) {
+            case "+": return a + b
+            case "-": return a - b
+            case "x": return a * b
+            case "/": return b === 0 ? "Erro" : a / b
+        }
+    }
     
 });
